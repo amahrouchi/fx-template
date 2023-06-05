3d285388895946ee5a279959477963d3b4c27879
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2023-06-05 16:11:25 +0200

    feat(recommendation): bind requested types using a bracketed name

--- app/recommendation/request/retailer_recommendation_request.go
+++ app/recommendation/request/retailer_recommendation_request.go
@@ -2,6 +2,6 @@ package recommendationRequest
 
 // RetailerRecommendationRequest model used to bind request data for retailer recommendations.
 type RetailerRecommendationRequest struct {
-	Types []int  `query:"types" validate:"required"`
+	Types []int  `query:"types[]" validate:"required"`
 	Lang  string `query:"lang" validate:"required,alpha,lowercase,len=2"`
 }
