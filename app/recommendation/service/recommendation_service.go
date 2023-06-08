7c883ff64b293962c64d87d9c5eefb675ba1361e
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2023-06-08 21:28:09 +0200

    feat(recommendation): parallelize API/cache calls w/ goroutines

--- app/recommendation/service/recommendation_service.go
+++ app/recommendation/service/recommendation_service.go
@@ -10,6 +10,13 @@ import (
 	"github.com/ekkinox/fx-template/modules/fxlogger"
 )
 
+// recommendationHolder is a struct holding recommendations and errors.
+type recommendationHolder struct {
+	typeId int
+	recos  any
+	err    error
+}
+
 // RecommendationService service gathering recommendations from different sources.
 type RecommendationService struct {
 	ttl int
@@ -27,144 +34,186 @@ func (r *RecommendationService) GetRecommendationByTypes(
 	recommendableType string,
 	typeIds []int,
 	lang string,
-) ([]any, error) {
+) []any {
 	// Get recommendations from client.
-	var recoByTypes []any
+	recoChannel := make(chan *recommendationHolder)
 	for _, typeId := range typeIds {
-		// Get cached recommendations
-		key := fmt.Sprintf("recommendations[%s][%d][%d][%s]", recommendableType, recommendableId, typeId, lang)
-		cachedRecos, err := r.cacheService.Get(key)
-		if err != nil {
-			return nil, err
+		go r.sendRecosToChan(recoChannel, recommendableType, recommendableId, typeId, lang)
+	}
+
+	// Wait for all goroutines gathering recommendations to finish
+	var recoByTypes []any
+	for recoHolder := range recoChannel {
+		if recoHolder.err != nil {
+			r.logger.Err(recoHolder.err).
+				Str("recommendableType", recommendableType).
+				Int("recommendableId", recommendableId).
+				Int("recommendationTypeId", recoHolder.typeId).
+				Str("lang", lang).
+				Msg("Unable to get recommendations.")
+
+			// Send empty recommendations on error
+			recoByTypes = append(recoByTypes, map[string]any{
+				"id":       recoHolder.typeId,
+				"entities": recoHolder.recos,
+				"error":    true,
+			})
+			continue
 		}
 
-		// Get products from api
-		// TODO:
-		//  - parallelize requests per type
-		switch typeId {
-		case recommendationEnum.RetailerProductsYouMayLike,
-			recommendationEnum.ProductProducts,
-			recommendationEnum.BrandProducts,
-			recommendationEnum.ProductProductsCrossBrand,
-			recommendationEnum.ProductProductsSameBrand:
-			// Unmarshal and return cached recommendations
-			if cachedRecos != "" {
-				var unmarshalledRecos []*recommendationModel.RecommendationProduct
-				err = json.Unmarshal([]byte(cachedRecos), &unmarshalledRecos)
-				if err != nil {
-					return nil, err
-				}
-
-				recoByTypes = append(recoByTypes, map[string]any{
-					"id":       typeId,
-					"entities": unmarshalledRecos,
-				})
-				continue
-			}
-
-			// Get recommendations from api
-			recommendationIds, err := r.recommendationApi.GetRecommendationsByEntityAndType(
-				recommendableId,
-				recommendableType,
-				typeId,
-				map[string]any{},
-			)
-			if err != nil {
-				return nil, err
-			}
-
-			// Get complete product data from api
-			products, err := r.productApi.GetMany(recommendationIds, lang)
-			if err != nil {
-				return nil, err
-			}
-
-			// Cache the complete product recommendations
-			jsonRecos, err := json.Marshal(products)
-			if err != nil {
-				return nil, err
-			}
-
-			err = r.cacheService.Set(key, string(jsonRecos), r.ttl)
-			if err != nil {
-				r.logger.Err(err).
-					Str("recommendableType", recommendableType).
-					Int("recommendableId", recommendableId).
-					Int("recommendationTypeId", typeId).
-					Str("lang", lang).
-					Msg("Unable to cache product recommendation.")
-				// Not returning an error here because we don't want to break the flow if the cache fails
-			}
-
-			// Add the products to the return array
-			recoByTypes = append(recoByTypes, map[string]any{
-				"id":       typeId,
-				"entities": products,
-			})
-
-		case recommendationEnum.RetailerBrandsYouMayLike,
-			recommendationEnum.RetailerBrandsCloseToYourArea,
-			recommendationEnum.ProductBrands,
-			recommendationEnum.BrandBrands:
-			// Unmarshal ann return cached recommendations
-			if cachedRecos != "" {
-				var unmarshalledRecos []*recommendationModel.RecommendationBrand
-				err = json.Unmarshal([]byte(cachedRecos), &unmarshalledRecos)
-				if err != nil {
-					return nil, err
-				}
-
-				recoByTypes = append(recoByTypes, map[string]any{
-					"id":       typeId,
-					"entities": unmarshalledRecos,
-				})
-				continue
-			}
-
-			// Get recommendations from api
-			recommendationIds, err := r.recommendationApi.GetRecommendationsByEntityAndType(recommendableId, recommendableType, typeId, map[string]any{})
-			if err != nil {
-				return []any{}, err
-			}
-
-			// Get complete product data from api
-			brands, err := r.brandApi.GetMany(recommendationIds)
-			if err != nil {
-				return nil, err
-			}
-
-			// Cache the complete product recommendations
-			jsonRecos, err := json.Marshal(brands)
-			if err != nil {
-				return nil, err
-			}
-
-			err = r.cacheService.Set(key, string(jsonRecos), r.ttl)
-			if err != nil {
-				r.logger.Err(err).
-					Str("recommendableType", recommendableType).
-					Int("recommendableId", recommendableId).
-					Int("recommendationTypeId", typeId).
-					Str("lang", lang).
-					Msg("Unable to cache product recommendation.")
-				// Not returning an error here because we don't want to break the flow if the cache fails
-			}
-
-			// Add the products to the return array
-			recoByTypes = append(recoByTypes, map[string]any{
-				"id":       typeId,
-				"entities": brands,
-			})
-
-		default:
-			message := fmt.Sprintf("Unhandled recommendation type %d", typeId)
-			r.logger.Warn().Int("typeId", typeId).Msg(message)
-			recoByTypes = append(recoByTypes, map[string]any{
-				"id":       typeId,
-				"entities": []any{},
-			})
+		recoByTypes = append(recoByTypes, map[string]any{
+			"id":       recoHolder.typeId,
+			"entities": recoHolder.recos,
+		})
+		if len(recoByTypes) == len(typeIds) {
+			close(recoChannel)
 		}
 	}
 
-	return recoByTypes, nil
+	return recoByTypes
+}
+
+// sendRecosToChan Sends cached or API recommendations to a channel.
+func (r *RecommendationService) sendRecosToChan(
+	recoChannel chan *recommendationHolder,
+	recommendableType string,
+	recommendableId int,
+	typeId int,
+	lang string,
+) {
+	// Get cached recommendations
+	key := fmt.Sprintf("recommendations[%s][%d][%d][%s]", recommendableType, recommendableId, typeId, lang)
+	cachedRecos, err := r.cacheService.Get(key)
+	if err != nil {
+		recoChannel <- &recommendationHolder{typeId, nil, err}
+		return
+	}
+
+	// Get products from api
+	switch typeId {
+	// Product recommendations
+	case recommendationEnum.RetailerProductsYouMayLike,
+		recommendationEnum.ProductProducts,
+		recommendationEnum.BrandProducts,
+		recommendationEnum.ProductProductsCrossBrand,
+		recommendationEnum.ProductProductsSameBrand:
+		// Unmarshal and return cached recommendations
+		if cachedRecos != "" {
+			var unmarshalledRecos []*recommendationModel.RecommendationProduct
+			err = json.Unmarshal([]byte(cachedRecos), &unmarshalledRecos)
+			if err != nil {
+				recoChannel <- &recommendationHolder{typeId, nil, err}
+				return
+			}
+
+			// Send cached product recommendations to channel
+			recoChannel <- &recommendationHolder{typeId, unmarshalledRecos, nil}
+			return
+		}
+
+		// Get recommendations from api
+		recommendationIds, err := r.recommendationApi.GetRecommendationsByEntityAndType(
+			recommendableId,
+			recommendableType,
+			typeId,
+			map[string]any{},
+		)
+		if err != nil {
+			recoChannel <- &recommendationHolder{typeId, nil, err}
+			return
+		}
+
+		// Get complete product data from api
+		products, err := r.productApi.GetMany(recommendationIds, lang)
+		if err != nil {
+			recoChannel <- &recommendationHolder{typeId, nil, err}
+			return
+		}
+
+		// Cache the complete product recommendations
+		jsonRecos, err := json.Marshal(products)
+		if err != nil {
+			recoChannel <- &recommendationHolder{typeId, nil, err}
+			return
+		}
+
+		err = r.cacheService.Set(key, string(jsonRecos), r.ttl)
+		if err != nil {
+			r.logger.Err(err).
+				Str("recommendableType", recommendableType).
+				Int("recommendableId", recommendableId).
+				Int("recommendationTypeId", typeId).
+				Str("lang", lang).
+				Msg("Unable to cache product recommendation.")
+			// Not sending an error to the channel here
+			// because we don't want to break the flow if the cache fails
+		}
+
+		// Send product recos to the channel
+		recoChannel <- &recommendationHolder{typeId, products, nil}
+
+	// Brand recommendations
+	case recommendationEnum.RetailerBrandsYouMayLike,
+		recommendationEnum.RetailerBrandsCloseToYourArea,
+		recommendationEnum.ProductBrands,
+		recommendationEnum.BrandBrands:
+		// Unmarshal and return cached recommendations
+		if cachedRecos != "" {
+			var unmarshalledRecos []*recommendationModel.RecommendationBrand
+			err = json.Unmarshal([]byte(cachedRecos), &unmarshalledRecos)
+			if err != nil {
+				recoChannel <- &recommendationHolder{typeId, nil, err}
+				return
+			}
+
+			// Send cached brand recos to the channel
+			recoChannel <- &recommendationHolder{typeId, unmarshalledRecos, nil}
+			return
+		}
+
+		// Get recommendations from api
+		recommendationIds, err := r.recommendationApi.GetRecommendationsByEntityAndType(recommendableId, recommendableType, typeId, map[string]any{})
+		if err != nil {
+			recoChannel <- &recommendationHolder{typeId, nil, err}
+			return
+		}
+
+		// Get complete product data from api
+		brands, err := r.brandApi.GetMany(recommendationIds)
+		if err != nil {
+			recoChannel <- &recommendationHolder{typeId, nil, err}
+			return
+		}
+
+		// Cache the complete product recommendations
+		jsonRecos, err := json.Marshal(brands)
+		if err != nil {
+			recoChannel <- &recommendationHolder{typeId, nil, err}
+			return
+		}
+
+		err = r.cacheService.Set(key, string(jsonRecos), r.ttl)
+		if err != nil {
+			r.logger.Err(err).
+				Str("recommendableType", recommendableType).
+				Int("recommendableId", recommendableId).
+				Int("recommendationTypeId", typeId).
+				Str("lang", lang).
+				Msg("Unable to cache product recommendation.")
+			// Not sending the error to the channel here
+			// because we don't want to break the flow if the cache fails
+		}
+
+		// Send brand recos to the channel
+		recoChannel <- &recommendationHolder{typeId, brands, nil}
+		return
+
+	default:
+		message := fmt.Sprintf("Unhandled recommendation type %d", typeId)
+		r.logger.Warn().Int("typeId", typeId).Msg(message)
+
+		// Send empty reco to the channel
+		recoChannel <- &recommendationHolder{typeId, []any{}, nil}
+		return
+	}
 }
